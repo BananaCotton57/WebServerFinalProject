2c1bb599db6e8e182781a98208860bf07cf0616b
BananaCotton57 <johnnyngo772@gmail.com>
2025-04-26 19:05:23 -0400

    users model works with supabase

--- server/index.js
+++ server/index.js
@@ -6,10 +6,23 @@ const exerciseTypesController = require('./controllers/exerciseTypesController')
 
 require('dotenv').config() // Load environment variables from .env file
 
-const PORT = 8000
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL); //debug
+console.log('SUPABASE_SECRET_KEY:', process.env.SUPABASE_SECRET_KEY); //debug
+
+const PORT = process.env.PORT ?? 8000
 
 const app = express();
-  app.use(express.json()) 
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  next()
+})
+  app.use(express.json())
 
 //Controllers
 //A controller is a function that handles the request and response objects.
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,43 +1,65 @@
-const data = require('../data/users.json');
+// Add dotenv config at the top to ensure environment variables are loaded
+require('dotenv').config();
 const { CustomError, statusCodes } = require('./errors');
-const { connect } = require("./supabase")
+const { connect } = require('./supabase');
 
-connect()
+const TABLE_NAME = 'users';
+
+// Helper function to get the base query
+const BaseQuery = () => connect().from(TABLE_NAME);
 
 async function getAll() {
+    const { data, error } = await BaseQuery().select('*');
+    
+    if (error) {
+        throw error;
+    }
+    
     return data;
 }
 
 async function get(id) {
-    const user = data.find(u => u.id == id);
-    if (!user) {
+    const { data, error } = await BaseQuery().select('*').eq('id', id);
+    
+    if (error) {
+        throw error;
+    }
+    
+    if (!data.length) {
         throw new CustomError('User not found', statusCodes.NOT_FOUND);
     }
-    return user;
+    
+    return data[0];
 }
 
 async function create(user) {
-    const newUser = {
-        id: data.length + 1,
-        ...user
-    };
-    data.push(newUser);
-    return newUser;
+    const { data, error } = await BaseQuery().insert(user).select('*');
+    
+    if (error) {
+        throw error;
+    }
+    
+    return data[0];
 }
 
 async function update(id, user) {
-    const index = data.findIndex(u => u.id == id);
-    if (index === -1) return null;
-    const updatedUser = { ...data[index], ...user };
-    data[index] = updatedUser;
-    return updatedUser;
+    const { data, error } = await BaseQuery().update(user).eq('id', id).select('*');
+    
+    if (error) {
+        throw error;
+    }
+    
+    return data[0];
 }
 
 async function remove(id) {
-    const index = data.findIndex(u => u.id == id);
-    if (index === -1) return null;
-    const deletedUser = data.splice(index, 1);
-    return deletedUser;
+    const { data, error } = await BaseQuery().delete().eq('id', id).select('*');
+    
+    if (error) {
+        throw error;
+    }
+    
+    return data[0];
 }
 
 module.exports = { getAll, get, create, update, remove };
